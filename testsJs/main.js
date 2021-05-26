9361229b277276817c08dad778254bbc061dad09
jayjay <jadersonrilidio@gmail.com>
2021-05-26 16:56:22 -0300

    fouders name changed, new lessons done

---
+++ testsJs/main.js
@@ -0,0 +1,6 @@
+let variaveis = [1, 2, 3, 4.555, 'Luiz'];
+const vetores = [12, 12, 23, ...variaveis];
+const nome = 'Luiz';
+const obj = { nome };
+const novoObj = { ...obj };
+console.log(novoObj);
